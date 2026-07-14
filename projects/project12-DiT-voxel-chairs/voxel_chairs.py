57af6b56bb5062c54ff67bd1713cfa551023804c
Decstar77 <34284628+Decstar77@users.noreply.github.com>
2026-07-15 01:41:19 +0200

    green

--- projects/project12-DiT-voxel-chairs/voxel_chairs.py
+++ projects/project12-DiT-voxel-chairs/voxel_chairs.py
@@ -5,9 +5,6 @@ from datasets import load_dataset
 # https://huggingface.co/datasets/ShapeNet/ShapeNetCore before downloading.
 dataset = load_dataset("ShapeNet/ShapeNetCore", name="v2", token=True)
 
-
-
-
 # To download to a specific directory
 # dataset = load_dataset("ShapeNet/ShapeNetCore", name="v2", cache_dir="./my_data")
 
