8c16e7a44d7270806210b60ac9fd34d459a3530c
Decstar77 <34284628+Decstar77@users.noreply.github.com>
2026-08-20 14:56:22 +0200

    Ga

--- projects/project16-basic-gaussians/src/main.cpp
+++ projects/project16-basic-gaussians/src/main.cpp
@@ -1,61 +1,84 @@
 #include "raylib.h"
 
 #include <glm/glm.hpp>
-#include <iostream>
-#include <math.h>
-#include <stdio.h>
 #include <string.h>
 
+/*
+===================
+===================
+*/
 struct OutputImage {
-    int         width;
-    int         height;
+    int width;
+    int height;
     glm::vec4 * pixels;
 };
 
-
-OutputImage CreateOutputImage(int w, int h) {
-    OutputImage image;
+/*
+===================
+===================
+*/
+OutputImage CreateOutputImage( int w, int h ) {
+    OutputImage image = {};
     image.width = w;
     image.height = h;
-    int size = w * h * sizeof( float ) * 4; 
-    image.pixels = ( glm::vec4 * ) malloc( size );
+    int size = w * h * sizeof( float ) * 4;
+    image.pixels = (glm::vec4 *) malloc( size );
     memset( image.pixels, 0, size );
     return image;
 }
 
-void PutPixel(OutputImage& image, int x, int y, glm::vec4 c) {
+/*
+===================
+===================
+*/
+void PutPixel( OutputImage & image, int x, int y, glm::vec4 c ) {
     int idx = y * image.width + x;
     image.pixels[idx] = c;
 }
 
-glm::vec4 GibPixel(OutputImage& image, int x, int y) {
+/*
+===================
+===================
+*/
+void AddPixel( OutputImage & image, int x, int y, glm::vec4 c ) {
+    int idx = y * image.width + x;
+    image.pixels[idx] += c;
+}
+
+/*
+===================
+===================
+*/
+glm::vec4 GibPixel( OutputImage & image, int x, int y ) {
     int idx = y * image.width + x;
     return image.pixels[idx];
 }
 
+/*
+===================
+===================
+*/
 struct Gaussian2D {
     glm::vec2 mu;
     glm::mat2 sigma;
     glm::vec4 color;
 };
 
-void SplatGaussian(OutputImage& image, const Gaussian2D & g) {
+/*
+===================
+===================
+*/
+void SplatGaussian( OutputImage & image, const Gaussian2D & g ) {
+    glm::mat2 sigmaInv = glm::inverse( g.sigma );
     for ( int y = 0; y < image.height; y++ ) {
         for ( int x = 0; x < image.width; x++ ) {
             glm::vec2 p = glm::vec2( x, y );
             glm::vec2 diff = p - g.mu;
-            glm::vec2 inv = glm::inverse( g.sigma ) * diff;
-            float res = glm::exp( -0.5f * glm::dot(diff, inv) );
-            if (res > 0.001f) {
-                int a = 2;
-            }
-            glm::vec4 col = g.color * res;
-            PutPixel( image, x, y, col );
+            float res = glm::exp( -0.5f * glm::dot( diff, sigmaInv * diff ) );
+            AddPixel( image, x, y, g.color * res );
         }
     }
 }
-
-
 
 /*
 ===================================================
@@ -73,26 +96,37 @@ Texture2D HdrToTexture( const OutputImage & src ) {
     img.width = src.width;
     img.height = src.height;
     img.mipmaps = 1;
-    img.format = PIXELFORMAT_UNCOMPRESSED_R8G8B8A8;
+    img.format = PIXELFORMAT_UNCOMPRESSED_R32G32B32A32;
 
     Texture2D tex = LoadTextureFromImage( img ); // uploads to GPU
     return tex;
 }
 
-
 int main() {
     const int screenWidth = 800;
     const int screenHeight = 800;
-    InitWindow( screenWidth, screenHeight, "SphericalHarmonics - SH projection explorer" );
+    InitWindow( screenWidth, screenHeight, "Basic Gaussian Splats" );
 
     OutputImage image = CreateOutputImage( screenWidth, screenHeight );
-    Gaussian2D g;
-    g.color = glm::vec4( 1, 1, 1, 1 );
-    g.mu = glm::vec2( screenWidth, screenHeight ) / 2.0f;
-    g.sigma = glm::mat2( 200 );
+
+    Gaussian2D g1;
+    g1.color = glm::vec4( 1, 0, 1, 1 );
+    g1.mu = glm::vec2( screenWidth + 200, screenHeight - 200 ) / 2.0f;
+    g1.sigma = glm::mat2( 60.0f * 60.0f );
+
+    Gaussian2D g2 = g1;
+    g2.color = glm::vec4( 1, 1, 0, 1 );
+    g2.mu = glm::vec2( screenWidth - 200, screenHeight - 200 ) / 2.0f;
+    
+    Gaussian2D g3 = g1;
+    g3.color = glm::vec4( 0, 1, 0, 1 );
+    g3.mu = glm::vec2( screenWidth, screenHeight + 100 ) / 2.0f;
+
+    SplatGaussian( image, g1 );
+    SplatGaussian( image, g2 );
+    SplatGaussian( image, g3 );
 
     Texture2D tex = HdrToTexture( image );
-    SplatGaussian( image, g );
 
     SetTargetFPS( 60 );
     while ( !WindowShouldClose() ) {
